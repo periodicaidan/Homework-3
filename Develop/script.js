93aa05d24465932eb48d518f16e6b677c8a35245
Aidan T. Manning <periodicaidan@gmail.com>
2020-09-23 17:06:02 -0500

    Commented the JS a bit

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,11 @@ const UPPERCASE = LOWERCASE.map(c => c.toUpperCase());
 const NUMERIC = '01234556789'.split('');
 const SPECIAL = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
 
+/**
+ * An object encapsulating all the applicable criteria for password generation
+ * 
+ * Shared mutable state is asking for trouble but for the purposes of this exercise it's fine
+ */
 let passwordCriteria = {
   length: 0,
   allowChars: {
@@ -37,6 +42,8 @@ let passwordCriteria = {
   },
 
   promptLength() {
+    // Since we're working with just this object in global scope, the length needs to be cleared 
+    // so that successive sessions don't inherit their length from the previous one
     this.length = 0;
     while (this.length < 8 || this.length > 128) {
       let input = prompt("How long shall the password be?\n(Must be between 8 and 128 inclusive)");
@@ -57,10 +64,15 @@ let passwordCriteria = {
   }
 }
 
+/**
+ * @param {typeof passwordCriteria} criteria 
+ */
 function generatePassword(criteria) {
+  // Ask user for criteria
   criteria.promptLength();
   criteria.promptFeatures();
 
+  // Generate password char-by-char in a loop
   let allowedChars = criteria.allowedChars;
   let password = "";
   for (let _i = 0; _i < criteria.length; ++_i) {
